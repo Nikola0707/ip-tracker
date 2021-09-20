f34f19a0f36820109ad4353aae8982c0c7a5c709
Nikola0707 <ristoski.nikola@gmail.com>
2021-09-20 15:18:27 +0200

    Fixing not showing map marker

--- src/components/Map/Map.component.jsx
+++ src/components/Map/Map.component.jsx
@@ -1,5 +1,17 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import Info from "../Info/Info.component";
+// React-leaflet
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map = () => {
   return (
